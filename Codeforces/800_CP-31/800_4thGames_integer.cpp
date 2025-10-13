299cb32ef24284b2360ff0eb2348447ad2f18efd
Samarth Manish Rathod <samarth9578@gmail.com>
2025-10-13 06:01:10 +0530

    Add codeforces solution - Games integer CP-31_800*

---
+++ Codeforces/800_CP-31/800_4thGames_integer.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+      int n;
+      cin>>n;
+      int cnt=0;
+      if(n%3==0)
+      {
+
+          cout<<"Second"<<endl;
+      }
+      else cout<<"First"<<endl;
+
+      }
+    }
