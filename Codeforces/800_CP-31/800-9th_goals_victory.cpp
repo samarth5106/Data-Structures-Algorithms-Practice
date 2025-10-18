9671e790d733ea48cfcc5791d5568b40146a2a43
Samarth Manish Rathod <samarth9578@gmail.com>
2025-10-18 12:05:00 +0530

    Add codeforces solution -Goals of victory 800*

---
+++ Codeforces/800_CP-31/800-9th_goals_victory.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+    int n;
+    cin>>n;
+    vector<int> A(n);
+    int sum=0;
+
+    for(int i=0;i<n-1;i++){
+        cin>>A[i];
+        sum+=A[i];
+    }
+    cout<<-1*sum<<endl;
+
+}
+}
