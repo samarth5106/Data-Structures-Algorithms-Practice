fd85657a3223055037a997ef921b714be8087335
Samarth Manish Rathod <samarth9578@gmail.com>
2025-10-29 23:54:19 +0530

    Add codechef solution-chocolate eating

---
+++ Codechef/0006_chocolate_eating.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    int sum=a+b;
+	    int wh=0,drk=0;
+	   if(a==b){
+	       cout<<a+b-1<<endl;
+	   }
+	   else{
+	       cout<<a+b<<endl;
+	   }
+	    
+	}
+
+}
