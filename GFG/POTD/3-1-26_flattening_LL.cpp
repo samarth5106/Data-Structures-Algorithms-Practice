f5d1e50770e195983f2cd98ce6f05bd110d624f4
Samarth Manish Rathod <samarth9578@gmail.com>
2026-01-03 17:33:00 +0530

    Add GFG POTD solution- flattening LL

---
+++ GFG/POTD/3-1-26_flattening_LL.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    Node* merge(Node* a, Node* b) {
+        if (!a) return b;
+        if (!b) return a;
+
+        Node* result;
+
+        if (a->data < b->data) {
+            result = a;
+            result->bottom = merge(a->bottom, b);
+        } else {
+            result = b;
+            result->bottom = merge(a, b->bottom);
+        }
+
+        result->next = NULL;
+        return result;
+    }
+
+    Node* flatten(Node* root) {
+        if (root == NULL || root->next == NULL)
+            return root;
+
+        root->next = flatten(root->next);
+
+        root = merge(root, root->next);
+
+        return root;
+    }
+};
