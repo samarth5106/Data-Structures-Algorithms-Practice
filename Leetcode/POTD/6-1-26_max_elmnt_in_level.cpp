1f4f18f23ca67fb568087fa7f16ee7cfec290dc8
Samarth Manish Rathod <samarth9578@gmail.com>
2026-01-06 07:38:40 +0530

    Add leetcode POTD solution- min sum in level of tree

---
+++ Leetcode/POTD/6-1-26_max_elmnt_in_level.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        if(root == NULL) return 0;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        int maxSum = INT_MIN;
+        int maxLevel = 1;
+        int currentLevel = 1;
+        
+        while(!q.empty()){
+            int levelSize = q.size();
+            long long levelSum = 0;
+            
+            // Process all nodes at current level
+            for(int i = 0; i < levelSize; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                
+                levelSum += node->val;
+                
+                // Add children to queue
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            
+            // Update max if current level sum is greater
+            if(levelSum > maxSum){
+                maxSum = levelSum;
+                maxLevel = currentLevel;
+            }
+            
+            currentLevel++;
+        }
+        
+        return maxLevel;
+    }
+};
