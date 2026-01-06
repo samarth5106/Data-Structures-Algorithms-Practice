727c09e2d775bc319ac98490910db52c55624c3a
Samarth Manish Rathod <samarth9578@gmail.com>
2026-01-06 23:03:31 +0530

    Add GFG POTD solution- max xor subarray of size k

---
+++ GFG/POTD/6-1-26_max_xor_subarray_of_size_k.cpp
@@ -0,0 +1,25 @@
+
+
+class Solution {
+  public:
+    int maxSubarrayXOR(vector<int>& arr, int k) {
+        // code here
+        int xr=0;
+        int maxxr=-1;
+        int l=0,r=k-1;
+        for(int i=0;i<=r;i++){
+            xr=xr^arr[i];
+        }
+        maxxr=max(xr,maxxr);
+        r++;
+        while(r<arr.size()){
+            xr=xr^arr[r];
+            xr=xr^arr[l];
+            l++;
+            r++;
+            maxxr=max(xr,maxxr);
+        }
+        return maxxr;
+    }
+};
+
