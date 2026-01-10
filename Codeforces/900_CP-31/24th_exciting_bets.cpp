3952d79e5c4f3152a06a24bc6c9ae436d448324c
Samarth Manish Rathod <samarth9578@gmail.com>
2026-01-10 21:16:01 +0530

    Add Codeforces CP-31 solution- exciting bets

---
+++ Codeforces/900_CP-31/24th_exciting_bets.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long a, b;
+        cin >> a >> b;
+        if (b > a)
+            swap(a, b);
+
+        if (a == b)
+            cout << 0 << " " << 0 << endl;
+        else
+        {
+            long long gcd = a - b;
+            long long moves = min(b % gcd, gcd - b % gcd);
+            cout << gcd << " " << moves << endl;
+        }
+    }
+    return 0;
+}
+
+
