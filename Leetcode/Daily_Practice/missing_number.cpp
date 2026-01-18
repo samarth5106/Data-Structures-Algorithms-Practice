8ce62cf317a6800cf876ff5b6d3942c047836f51
Samarth Manish Rathod <samarth9578@gmail.com>
2026-01-18 18:00:24 +0530

    Add Leetcode solution - missing number

---
+++ Leetcode/Daily_Practice/missing_number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==ans)
+            ans++;
+        }
+        return ans;
+    }
+};
