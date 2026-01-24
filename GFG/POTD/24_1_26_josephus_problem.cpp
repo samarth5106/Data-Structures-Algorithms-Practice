4eb21edfdfc67ff16c5f7f334e63f0b465610ad6
Samarth Manish Rathod <samarth9578@gmail.com>
2026-01-25 02:52:21 +0530

    describe the reason of why my previous approach failed for the same problem

--- GFG/POTD/24_1_26_josephus_problem.cpp
+++ GFG/POTD/24_1_26_josephus_problem.cpp
@@ -40,3 +40,5 @@ class Solution {
         return i;
     }
 }
+see my approac was this just refined by AI, wht i was doing was k=k%n, thinking what if k>n so we can do k%n=k,, but this was wrong we need to eliminate kth alive personn wach time so after eliminarting, the count is decreasing by 1 as we eliminated that person, so hmara ye appraoch of doing k%n is ok only or round 1 not everytime,,as after each time n is decreasing by 1,, and moreover what if k==, then k%n will be 0,, means we have to eliminate 0th alive person that makes no sense.
+
