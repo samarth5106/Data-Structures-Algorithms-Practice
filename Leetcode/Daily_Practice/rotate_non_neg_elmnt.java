4599432c1023bef4f6e80a6b2da57f06e295c166
Samarth Manish Rathod <samarth9578@gmail.com>
2026-01-25 12:09:39 +0530

    Describe the fault in my previous bruteforce solution which was giving TLE

--- Leetcode/Daily_Practice/rotate_non_neg_elmnt.java
+++ Leetcode/Daily_Practice/rotate_non_neg_elmnt.java
@@ -33,3 +33,12 @@ class Solution {
         return nums;
     }
 }
+See what i was doing- rotating manually like for every value till we not 
+reached k, I was rotating every single elmnt to its left ,, so outer 
+loop - while(k-->0) and inner loop again inner loop for shiting elmnt to 
+their left place ,,, so got TLE.
+ Optimise- k=k%cnt,, to handel if the value of k is greater than cnt ,, 
+ then we did not sihifted each elmnt to its left but we directly finded 
+ the final position after that ith elmnt got shifted by k ,, by formula
+rotation[i]=position[(i+k)%cnt]
+cnt - number of positives, 
