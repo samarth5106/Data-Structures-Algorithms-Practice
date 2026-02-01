129a153fe3a452195a7ff782dae13d2015202c69
Samarth Manish Rathod <samarth9578@gmail.com>
2026-02-01 17:19:14 +0530

    Add Leetcode solution - final element after subarray deletions

---
+++ Leetcode/Daily_Practice/final_element_after_subarray_deletions.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int finalElement(int[] nums) {
+        return Integer.max(nums[0],nums[nums.length-1]);
+    }
+}
+see it is leetcode 3828 que
+see Alice start kr rha hai if the max element middle of array me hai then Bob usse remove kr sakta hai so bob ka chance hi mat aane do therefore 1st and last element , dono me se jo bada hai use return kr do game  khatam
