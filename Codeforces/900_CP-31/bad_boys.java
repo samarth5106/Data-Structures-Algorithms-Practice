a599e5eca593879a425540fb226ed43824a0b339
Samarth Manish Rathod <samarth9578@gmail.com>
2026-02-04 00:37:21 +0530

    Add Codeforces solution - bad boys

---
+++ Codeforces/900_CP-31/bad_boys.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class Codechef
+{
+    public static void main(String[] args)
+    {
+       Scanner sc=new Scanner(System.in);
+       int t;
+       t=sc.nextInt();
+       while(t-->0)
+       {
+           int n,m,i,j;
+           n=sc.nextInt();
+           m=sc.nextInt();
+           i=sc.nextInt();
+           j=sc.nextInt();
+           
+           System.out.println(n+ " "+"1"+" "+"1"+" "+m);
+       }
+       
+
+    }
+}
