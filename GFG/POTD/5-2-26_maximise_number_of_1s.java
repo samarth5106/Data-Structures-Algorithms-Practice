ff305c6089b8484edd0a16cf786e513d163d54f3
Samarth Manish Rathod <samarth9578@gmail.com>
2026-02-05 23:58:05 +0530

    Describe the approach of solving GFG POTD

--- GFG/POTD/5-2-26_maximise_number_of_1s.java
+++ GFG/POTD/5-2-26_maximise_number_of_1s.java
@@ -17,3 +17,6 @@ class Solution{
         return best;
     }
 }
+
+
+see approach is to maintain a window and uss window ke andr numbers of zeros count karo the moment number of zero becomes greater than k then l ko aage badhao and usme bhi condition lagao ki if arr[l]==0 hai then zero ke counter ko bhi minus 1 krenge and l++,,,, see jab zerocount<=k hai so window valid hai so we have stored the size best=max(best,r-l+1),, 
