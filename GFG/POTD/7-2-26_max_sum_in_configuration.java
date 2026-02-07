5801c8ecfcc3342597b91343a73c410630168d69
Samarth Manish Rathod <samarth9578@gmail.com>
2026-02-07 23:39:20 +0530

    describe your approach/logic

--- GFG/POTD/7-2-26_max_sum_in_configuration.java
+++ GFG/POTD/7-2-26_max_sum_in_configuration.java
@@ -13,4 +13,10 @@ class Solution{
         }
         return (int)ans;
     }
-}
+Approach- see ek thought tha ki if array ke max elmnt ko max 
+arr.length-1th indx de and baaki ke array me circular way me iterate 
+krte hue unhe remaining indces de o max sum mil skta hai as we gave the 
+max elmnt the max index possible , but in array there can be duplicates 
+so in that case this wont work
+
+My method- given array ko rotate vegere kr jo permutations bnte he un sb ken lie sum krke dekhte hai but its BRUTEFORCE so TLE ,, so other thought was ki array ke elmnts ko vaise hi rkhte hai and only indices ko rotate krte hai and then observing and trying to find some pattern like on moving from moving from 1 permutation/pattern to next pattern the sum jo aaega usme hm kisi formula se nikal paaenge ,, so thi swas the ase of approach
