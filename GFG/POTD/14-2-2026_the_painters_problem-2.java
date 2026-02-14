6dde9b1e151961e25b137730a7c117896cea3aab
Samarth Manish Rathod <samarth9578@gmail.com>
2026-02-15 00:52:20 +0530

    Describe the logic, intuition, approach used

--- GFG/POTD/14-2-2026_the_painters_problem-2.java
+++ GFG/POTD/14-2-2026_the_painters_problem-2.java
@@ -38,3 +38,10 @@ class Solution {
             return low;
     }
 }
+
+
+Logic - Binary search + Greedy 
+
+see minimum time jo lag sakta hai is the max(arr) means when the numbers of painters (k) would be equal to the size of array then each painter will paint one board,,, and the maximum answer possible will be the sum of all elements means when only one painter is there (k=1),, so low = max(arr) and high=sum(arr),, issme BS lagao to find the minimum possible time req to paint all the boards , so  apan ne mid nikala then GREEDILY check that ye mid ko complete karne me kitne painters lagenge if painters required will be greater than k then low=mid+1,, else it will mean ki ho sakta ho mid ki value aur ki jaa sakti hai so then high=mid-1 krenge 
+
+Greedy - we got value of mid ,, so now ek painter ko assign karo boards (elements of array) as much as possible till its sum is less than mid ,, and when limit is reched then we will increase painters(count of number of painters) by 1 and check whether this count is less than k or not ,, if not then return false
