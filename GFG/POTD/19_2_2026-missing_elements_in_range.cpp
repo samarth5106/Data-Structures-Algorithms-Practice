e979e9df9913e84e7e98af9b37d6f2e31d597fca
Samarth Manish Rathod <samarth9578@gmail.com>
2026-02-19 22:34:47 +0530

    Add GFG POTD solution - missing element in range

---
+++ GFG/POTD/19_2_2026-missing_elements_in_range.cpp
@@ -0,0 +1,54 @@
+class Solution {
+  public:
+    vector<int> missinRange(vector<int>& arr, int low, int high) {
+        // code here
+        sort(arr.begin(),arr.end());
+        vector<int> ans;
+        int val=low;
+        bool done=false;
+        bool hai=false;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==high){
+                hai=true;
+            }
+            if(arr[i]>high)
+            break;
+            
+            if(arr[i]<val)
+            continue;
+            
+            else if(arr[i]==val){
+                val++;
+                if(val>high){
+                    done=true;break;
+                }
+                
+            }
+            if(arr[i]>val){
+                while(val<high&&arr[i]>val){
+                    ans.push_back(val);
+                    val++;
+                }
+              // val++;
+               if(val>high)
+               {
+                   done=true;
+                   break;
+               }
+               else if(arr[i]<val)
+               continue;
+               else{
+                   val++;
+               }
+            }
+            
+        }
+        if(!hai){
+            while(val<=high){
+                ans.push_back(val);
+                val++;
+            }
+        }
+        return ans;
+    }
+};
