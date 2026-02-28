d0856cb19e0c8d79d1e84bfb808ba4e8936c8a39
Samarth Manish Rathod <samarth9578@gmail.com>
2026-02-28 14:19:37 +0530

    Describe why your initial code failed and what is the correct approach

--- Codeforces/1000_CP-31/10th_traffic_light.cpp
+++ Codeforces/1000_CP-31/10th_traffic_light.cpp
@@ -48,3 +48,15 @@ int main() {
     }
 
 }
+
+
+My First approach - see maine pahle jaisa bola gaya tha vaisa Implementation kia which Got me TLE ,,
+
+Correct Approach - see the correct way is to use Binary Search see we need to find the maximum distance from any c to next g (circularly) ,, we are using binary search we want
+to quckly find the next 'g' after it so if we store alll indices of g in a array nsorted way mein then for a position i we can use binary search to find the index in which g is 
+present and its index is also greater than i,,
+
+so make string double ,, that is s=s+s,, so circular issue solve ho gya ,,
+then, now store all positions of g in vector 'green' ,, for every positions i where s[i]==c apply Binary search in that 'green' vectorfind first index of 'g' >i
+
+and the distance from the index of c (i) will be green_index-i
