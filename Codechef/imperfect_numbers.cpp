2e28c85deb8eb2b878fb11ca6486ff52ddc22589
Samarth Manish Rathod <samarth9578@gmail.com>
2026-03-25 23:19:42 +0530

    Add codechef solution - imperfect number

---
+++ Codechef/imperfect_numbers.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%2==0&&n%5==0){
+            cout<<'2'<<endl;
+        }
+        else if(n%2==0||n%5==0){
+            cout<<'0'<<endl;
+        }
+        else if(n%2==1){
+            cout<<'1'<<endl;
+        }
+    
+    }
+
+}
