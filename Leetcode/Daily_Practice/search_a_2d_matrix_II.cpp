3dd3de85fb4c526f523d63611a35597c7e634f0a
Samarth Manish Rathod <samarth9578@gmail.com>
2026-04-05 01:45:55 +0530

    Add Leetcode solution - search a 2D matrix II

---
+++ Leetcode/Daily_Practice/search_a_2d_matrix_II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int m=mat.size();
+        int n=mat[0].size();
+        int r=0,c=n-1;
+       while(r<m&&c>=0) {
+            if(mat[r][c]==target) return true;
+            else if(mat[r][c]>target) c--;
+            else r++;
+        }
+        return false;
+    }
+};
