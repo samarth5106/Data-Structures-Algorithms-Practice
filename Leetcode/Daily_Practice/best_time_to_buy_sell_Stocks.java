873f0823dd13cbb9b6ac7f30431080c70b17d04a
Samarth Manish Rathod <samarth9578@gmail.com>
2026-06-16 15:10:00 +0530

    Add Leetcode solution - best time to buy and sell stocks

---
+++ Leetcode/Daily_Practice/best_time_to_buy_sell_Stocks.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxi=0;
+        int mini=prices[0];
+        for(int i=0;i<prices.length;i++){
+            mini=Math.min(mini,prices[i]);
+            maxi=Math.max(maxi,prices[i]-mini);
+        }
+        return maxi;
+        
+        
+    }
+}
