252464931c33df40aab14b123d1fa32c61576fec
Samarth Manish Rathod <samarth9578@gmail.com>
2026-06-18 22:23:56 +0530

    Add Leetcode solution - valid parentheses

---
+++ Leetcode/Daily_Practice/valid_parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c:s){
+            if(st.empty()) st.push(c);
+            else{
+                if(c==')'&&st.top()=='(') st.pop();
+                else if(c=='}'&&st.top()=='{') st.pop();
+                else if(c==']'&&st.top()=='[')st.pop();
+                else{
+                    st.push(c);
+                }
+            }
+        }
+        return st.empty();
+    }
+};
