66b1113c3dfec0ce959b7430b86e5fbdf9484676
Samarth Manish Rathod <samarth9578@gmail.com>
2026-09-02 23:19:55 +0530

    Add LC POTD solution - construct uniform paity array 1

---
+++ Leetcode/POTD/2-9-2026_construct_uniform_parity_array_1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        //odd- odd= even
+        //odd - even = odd
+
+
+        //even - odd = odd
+        //even - even =even
+        // if 1 odd then no else yes
+      //  int cnt_odd=0;
+        return true;
+
+    }
+}
